c6a0f2d5d51673b574d7c1c13d2160188db2e3d6
Charles Earl <charles.cearl@gmail.com>
2023-11-02 10:02:04 -0400

    Setup

---
+++ llama-rags/simple-search.py
@@ -0,0 +1,14 @@
+from llama_index import (
+    SimpleDirectoryReader,
+    VectorStoreIndex,
+    ServiceContext,
+)
+from llama_index.llms import LlamaCPP
+from llama_index.llms.llama_utils import (
+    messages_to_prompt,
+    completion_to_prompt,
+)
+
+model_url = "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/llama-2-13b-chat.ggmlv3.q4_0.bin"
+
+
